2cffe5e0d832aaaaba5344bc4329197ab07a98b1
Manjitha <manjitha.ee@gmail.com>
2023-05-17 14:27:10 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.DataFrame({'recID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+df = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
                     'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C']})
 
@@ -12,24 +12,37 @@ def calc_StartProbability(attri,var):
 
     return occurence/no_records # Returning start probability
 
-def calc_ObservationProbability():
+def calc_ObservationProbability(userID,attri):
+    df2 = df.loc[df['userID'] == 2]
+    var = df2.loc[:, attri]         # Obtaining the value of the given attribute and given userID
+    print(var)
+    occurence = df[attri].value_counts()[var]  # Calculating the number of occurences of the specific value
+
+    return (1-1/occurence)
+
+
+
+
+
+
+
+
+def calc_MMRisk(userID,attr):
     pass
 
 
 
-def calc_MMRisk(recID,attr):
-    pass
 
 
 
 
+#print(calc_StartProbability('a1',2015))
+#print(calc_StartProbability('a1',2016))
+#print(calc_StartProbability('a1',2017))
 
+#print(calc_ObservationProbability(1,'a1'))
+#print(calc_ObservationProbability(2,'a1'))
 
-
-print(calc_StartProbability('a1',2015))
-print(calc_StartProbability('a1',2016))
-print(calc_StartProbability('a1',2017))
-
-
-
-
+df2 = df.loc[df['userID'] == 3]
+df3 = df2['a1']
+print(df3)
