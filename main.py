ebed4b17f534ef30fa9f2068b2c4850e44af21e0
Manjitha <manjitha.ee@gmail.com>
2023-05-19 16:40:31 +0200

    Improving readability

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ sensitive_attri = ['a1','a2']
 def calc_StartProbability(userID,attri):
 
     # Calculate the start probability of a given attribute and UserID
-
+    no_records = df.shape[0]  # Calculating the number of records of the dataset
     index = userID - 1  # This is not always right. TO DO : Do the changes when the same userID repeats
-    val = df.loc[index][attri]
-    no_records = df.shape[0]      #Calculating the number of records of the dataset
-    occurence = df[attri].value_counts()[val] # Calculating the number of occurences of the specific value
+    val = df.loc[index][attri]  # Calculating the value of the given attribute for the given userID
 
-    return occurence/no_records # Returning start probability
+    occurrence = df[attri].value_counts()[val] # Calculating the number of occurrences of the specific value
+
+    return occurrence/no_records # Returning start probability
 
 def calc_ObservationProbability(userID,attri):
     df2 = df.loc[df['userID'] == userID]
     index = userID-1       # This is not always right. TO DO : Do the changes when the same userID repeats
     val = df2.loc[index, attri]         # Obtaining the value of the given attribute and given userID
-    #print(var)
-    occurence = df[attri].value_counts()[val]  # Calculating the number of occurences of the specific value
 
-    return (1-1/occurence)
+    occurrence = df[attri].value_counts()[val]  # Calculating the number of occurences of the specific value
+
+    return (1-1/occurrence)
 
 def calc_TransitionProbability(userID):
     # Calculates P(vj+1/vj )
@@ -35,24 +35,17 @@ def calc_TransitionProbability(userID):
     val1 = df.loc[index]['a1']
     val2 = df.loc[index]['a2']
 
-    occurence_attri1 = df['a1'].value_counts()[val1]  # Calculating the number of occurences of the specific value
-    #print(occurence_attri1)
+    occurrence_attri1 = df['a1'].value_counts()[val1]  # Calculating the number of occurrences of the specific value
 
     df3 = df.loc[df['a1'] == val1]
-    occurence_attri2 = df3['a2'].value_counts()[val2]  # Calculating the number of occurences of the specific value
-    #print(df3)
-    #print(occurence_attri2)
+    occurrence_attri2 = df3['a2'].value_counts()[val2]  # Calculating the number of occurrences of the specific value
 
-    return occurence_attri2/occurence_attri1
-
-
-
+    return occurrence_attri2/occurrence_attri1
 
 def calc_MMRisk(userID,attri):
+    attri1 = attri          # Assigning the passed attribute
 
-    attri1 = attri
-
-    for i in sensitive_attri:
+    for i in sensitive_attri:      # Assigning the other attribute
         if attri1 != i:
           attri2 = i
 
@@ -65,14 +58,6 @@ def calc_MMRisk(userID,attri):
 
 
 ########  TEST ########
-
-#print(calc_StartProbability('a1',2015))
-#print(calc_StartProbability('a1',2016))
-#print(calc_StartProbability('a1',2017))
-
-#print(calc_ObservationProbability(1,'a1'))
-#print(calc_ObservationProbability(4,'a1'))
-#print(calc_ObservationProbability(10,'a1'))
 
 """
 for i in range (1,11):
