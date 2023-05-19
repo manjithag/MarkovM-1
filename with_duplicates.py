ed7318d4ecaf2782004ab8d856aef6148fff54f7
Manjitha <manjitha.ee@gmail.com>
2023-05-19 17:44:56 +0200

    Updating Observation Probability

--- with_duplicates.py
+++ with_duplicates.py
@@ -8,25 +8,27 @@ df = pd.DataFrame({'userID': [1, 1, 1, 4, 5, 6, 7, 8, 9, 10],
 
 sensitive_attri = ['a1','a2']
 
-def calc_StartProbability(userID,attri):
+def calc_StartProbability(index,attri):
+    # Calculate the start probability of a given attribute and index
 
-    # Calculate the start probability of a given attribute and UserID
-    no_records = df.shape[0]  # Calculating the number of records of the dataset
-    index = userID - 1  # This is not always right. TO DO : Do the changes when the same userID repeats
-    val = df.loc[index][attri]  # Calculating the value of the given attribute for the given userID
+    no_records = df.shape[0]        # Counting the number of records of the dataset
+    val = df.loc[index][attri]      # Obtaining the value of the given attribute for the given index
+    occurrence = df[attri].value_counts()[val]      # Calculating the number of occurrences of the specific value
 
-    occurrence = df[attri].value_counts()[val] # Calculating the number of occurrences of the specific value
+    return occurrence/no_records           # Returning start probability
 
-    return occurrence/no_records # Returning start probability
+def calc_ObservationProbability(index,attri):
 
-def calc_ObservationProbability(userID,attri):
-    df2 = df.loc[df['userID'] == userID]
-    index = userID-1       # This is not always right. TO DO : Do the changes when the same userID repeats
+    attri_val = df.loc[index][attri]
+    userID  = df.loc[index]['userID']
+
+    df2 = df.loc[df[attri] == attri_val]
+    no_records = df2.shape[0]            # Counting the number of records of the dataset
     val = df2.loc[index, attri]         # Obtaining the value of the given attribute and given userID
 
-    occurrence = df[attri].value_counts()[val]  # Calculating the number of occurences of the specific value
+    occurrence = df2['userID'].value_counts()[userID]  # Calculating the number of occurences of the specific value
 
-    return (1-1/occurrence)
+    return (1-occurrence/no_records)
 
 def calc_TransitionProbability(userID):
     # Calculates P(vj+1/vj )
@@ -59,12 +61,14 @@ def calc_MMRisk(userID,attri):
 
 ########  TEST ########
 
-"""
-for i in range (1,11):
-    print(calc_StartProbability(i,'a1'))
-    #print(calc_ObservationProbability(i, 'a1'))
+
+for i in range (10):        # Looping by index of the dataset
+    #print(calc_StartProbability(i,'a1'))
+    print(calc_ObservationProbability(i, 'a1'))
     #print(calc_ObservationProbability(i, 'a2'))
     #print(calc_TransitionProbability(i))
+
+
 
 """
 for i in range (1,11):
@@ -75,4 +79,4 @@ print('\n')
 for i in range (1,11):
     print(calc_MMRisk(i,'a2'))
 
-
+"""
