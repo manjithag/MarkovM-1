92e6d02ee9e281d3297f04421b9c0a96a51be4ed
Manjitha <manjitha.ee@gmail.com>
2023-05-19 19:33:54 +0200

    adding 3 test cases

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 import pandas as pd
 from with_duplicates import calc_MMRisk,calc_StartProbability,calc_ObservationProbability,calc_TransitionProbability
 
-df = pd.DataFrame({'userID': [1, 1, 1, 4, 5, 6, 7, 8, 9, 10],
+
+## Test Case 1 : Without duplicates of users
+df1 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
                     'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C']})
 
-sensitive_attri = ['a1','a2']
+## Test Case 2 : With duplicates of users
+df2 = pd.DataFrame({'userID': [1, 1, 1, 4, 5, 6, 7, 8, 9, 10],
+                    'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
+                    'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C']})
+
+## Test Case 3 : With duplicates of users containing different attribute values
+df3 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 1, 1, 7, 8],
+                    'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
+                    'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C']})
+
+sensitive_attri = ['a1','a2']    # Valid for all test cases
 
 ########  TEST ########
 
