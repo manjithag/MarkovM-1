e632a6999c2123d784a96f86df3017a090ad49e3
Manjitha <manjitha.ee@gmail.com>
2023-05-20 12:43:53 +0200

    Updated calc_MMRisk and calc_TransitionProbability for handling many attributes more than 2.

--- mmRisk_manySAs.py
+++ mmRisk_manySAs.py
@@ -38,15 +38,9 @@ def create_Risk_Dataframe(df:pd.DataFrame,sensitive_attri:list):
 
         return (1-occurrence/no_records)
 
-    def calc_TransitionProbability(index,attri):
+    def calc_TransitionProbability(index,attri1,attri2):
         # Calculates the transition probability from given attribute to next attribute
         # --> P(attri2 = attri2_val / attri1 = attri1_val)
-
-        attri1 = attri        # Assigning the passed attribute (The attribute relevant to the risk of re-identification
-
-        for i in sensitive_attri:  # Assigning the other attribute
-            if attri1 != i:
-                attri2 = i
 
         val_attri1 = df.loc[index][attri1]
         val_attri2 = df.loc[index][attri2]
@@ -61,30 +55,36 @@ def create_Risk_Dataframe(df:pd.DataFrame,sensitive_attri:list):
     def calc_MMRisk(userID,attri):
         # Calculates the Markov Model Risk for a given record by multiplying Start, Observation & Transition probabilities
 
-        attri1 = attri          # Assigning the passed attribute
+        #attri1 = attri          # Assigning the passed attribute
 
-        for i in sensitive_attri:      # Assigning the other attribute
-            if attri1 != i:
-              attri2 = i
+        #index_attri1 = sensitive_attri.index(attri1)
+        #attri2 = sensitive_attri[index_attri1 + 1]
 
-        joint_prob = calc_StartProbability(userID,attri1)*calc_ObservationProbability(userID,attri1)*calc_TransitionProbability(userID,attri1)*calc_ObservationProbability(userID,attri2)
+        joint_p1 = calc_StartProbability(userID,attri) * calc_ObservationProbability(userID,attri)
+        joint_p2 = 1
+        for i in range(len(sensitive_attri)-1):
+
+            attri1 = sensitive_attri[i]
+            attri2 = sensitive_attri[i+1]
+
+            joint_p2 *= calc_TransitionProbability(userID,attri1,attri2) * calc_ObservationProbability(userID,attri2)
+
+        joint_prob = joint_p1 * joint_p2
         MMRisk = 1 - joint_prob
         MMRisk = round(MMRisk,3)        # Round off the value of MMRisk to 3 decimal points
 
         return MMRisk
 
-    risk_list = []                      # List of lists to store calculated risks for every index and SA
+    risk_list = []                      # List to store calculated risks for every index
 
-    for j in range(len(sensitive_attri)):       # Looping for every SA
-        risks = []
-        for i in range(df.shape[0]):            # Looping for every index
-            risks.append(calc_MMRisk(i,sensitive_attri[j]))
-        risk_list.append(risks)
+    for i in range(df.shape[0]):            # Looping for every index
+        risk_list.append(calc_MMRisk(i,sensitive_attri[0]))     # Starting from the first attribute
 
-    #print(risk_list)
-    risk_df = pd.DataFrame(risk_list, sensitive_attri).T   # .T is used to obtain the transpose of the dataframe
 
-    return risk_df
+    print(risk_list)
+    #risk_df = pd.DataFrame(risk_list, 'PR').T   # .T is used to obtain the transpose of the dataframe
+
+    #return risk_df
 
 
 
--- test_manySAs.py
+++ test_manySAs.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from mmRisk_2SA import create_Risk_Dataframe
+from mmRisk_manySAs import create_Risk_Dataframe
 
 ## Test Case 1 : Without duplicates of users
 df1 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
@@ -19,15 +19,18 @@ df3 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 1, 1, 7, 8],
 ## Test Case 4 : 3 nos sensitive attributes without duplicates of users
 df4 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
-                    'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C']
+                    'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C'],
                     'a3': [33, 33, 37, 38, 33, 39, 34, 34,  35, 35]})
 
-
-sensitive_attri = ['a1','a2','a3']    # Valid for all test cases (Shortly known as SA)
+sensitive_attri1 = ['a1','a2']    # Valid for all test cases (Shortly known as SA)
+sensitive_attri2 = ['a1','a2','a3']    # Valid for test case 4 (Shortly known as SA)
 
 ########  TEST ########
 
-#print(create_Risk_Dataframe(df1,sensitive_attri))
+#print(create_Risk_Dataframe(df1,sensitive_attri1))
 #print(create_Risk_Dataframe(df2,sensitive_attri))
 #print(create_Risk_Dataframe(df3,sensitive_attri))
-print(create_Risk_Dataframe(df4,sensitive_attri))
+#print(create_Risk_Dataframe(df4,sensitive_attri2))
+
+create_Risk_Dataframe(df4,sensitive_attri2)
+
