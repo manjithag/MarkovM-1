69f6fba2cb626ad2f061c6b5669c29b96721ad76
Manjitha <manjitha.ee@gmail.com>
2023-05-20 10:57:09 +0200

    Add test case 4 with 3 nos sensitive attributes

--- test_manySAs.py
+++ test_manySAs.py
@@ -16,10 +16,18 @@ df3 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 1, 1, 7, 8],
                     'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
                     'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C']})
 
-sensitive_attri = ['a1','a2']    # Valid for all test cases (Shortly known as SA)
+## Test Case 4 : 3 nos sensitive attributes without duplicates of users
+df4 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
+                    'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C']
+                    'a3': [33, 33, 37, 38, 33, 39, 34, 34,  35, 35]})
+
+
+sensitive_attri = ['a1','a2','a3']    # Valid for all test cases (Shortly known as SA)
 
 ########  TEST ########
 
-print(create_Risk_Dataframe(df1,sensitive_attri))
-print(create_Risk_Dataframe(df2,sensitive_attri))
-print(create_Risk_Dataframe(df3,sensitive_attri))
+#print(create_Risk_Dataframe(df1,sensitive_attri))
+#print(create_Risk_Dataframe(df2,sensitive_attri))
+#print(create_Risk_Dataframe(df3,sensitive_attri))
+print(create_Risk_Dataframe(df4,sensitive_attri))
