4f5a4c68188ff0d8aff5d134505c352d08fad4fd
Manjitha <manjitha.ee@gmail.com>
2023-05-23 13:37:00 +0200

    Added workflow.py & test_workflow.py

---
+++ test_workflow.py
@@ -0,0 +1,2 @@
+## This test the workflow.py
+
---
+++ workflow.py
@@ -0,0 +1,10 @@
+'''
+This workflow finds the ordered attributes o1,⋯, om that gives the highest mean, median, maximum or marketer risk.
+The same workflow is applicable to calculate the worst-case median,marketer, and maximum risks as well
+
+only considers the potential combinations that would give the highest risk and prunes the unnecessary combinations
+of attributes with lower risks.
+'''
+
+
+
