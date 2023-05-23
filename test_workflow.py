00098ec3e936ac9bcf797c40014023e0adf6e1bb
Manjitha <manjitha.ee@gmail.com>
2023-05-23 14:58:58 +0200

    Added calc_single_attribute_risks to calculate the highest mean value of single attribute risks.
    mmRisk_manySAs-Copy.py is a backup

--- test_workflow.py
+++ test_workflow.py
@@ -1,2 +1,20 @@
 ## This test the workflow.py
 
+import pandas as pd
+from workflow import find_workflow
+
+## Test Case 1 : 4 nos attributes
+df1 = pd.DataFrame({'userID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    'a2': ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'A',  'A', 'C'],
+                    'a1': [2015, 2015, 2015, 2016, 2016, 2016, 2016, 2016, 2016, 2017],
+                    'a3': ['aa', 'bb', 'cc', 'aa', 'aa', 'cc', 'cc', 'cc',  'cc', 'cc'],
+                    'a4': [33, 33, 37, 38, 33, 39, 34, 34,  35, 35]})
+
+attributes1 = ['a1','a2','a3','a4']
+attributes2 = ['a3','a1','a4','a2']
+
+find_workflow(df1,attributes1)
+find_workflow(df1,attributes2)
+
+
+
--- workflow.py
+++ workflow.py
@@ -5,6 +5,48 @@ The same workflow is applicable to calculate the worst-case median,marketer, and
 only considers the potential combinations that would give the highest risk and prunes the unnecessary combinations
 of attributes with lower risks.
 '''
+import pandas as pd
+from mmRisk_manySAs import create_Risk_Dataframe
+
+
+def calc_correlation (attri1, attri2):
+    #' This returns the Cramer's V Correlation scores between the given attributes of attri1 & attri2
+    pass
+
+def find_workflow(df : pd.DataFrame, attributes : list):
+    ## This finds the ordered attributes o1,⋯, om that gives the highest mean, median, maximum or marketer risk.
+
+    ## 1. Calculate the single attribute risks.
+    def calc_single_attribute_risks(attributes):
+        # Calculate the single attribute risks of a given attribute list
+
+        no_records = df.shape[0]        # Counting the number of records of the dataset
+        single_attribute_risks = []
+
+        for i in attributes:
+            start_prob = 0
+
+            for j in range (no_records):
+                attri_val = df.loc[j][i]              # Obtaining the value of the given attribute for the given index
+                occurrence = df[i].value_counts()[attri_val]     # Calculating the number of occurrences of the specific value
+                start_prob += occurrence / no_records       # Returning start probability
+
+            single_attribute_risks.append(start_prob/no_records)   # Appending with mean single attribute risk
+
+        single_attribute_risks = [round(num, 4) for num in single_attribute_risks]
+        max_single_attri_risk = max(single_attribute_risks)     # Highest single attribute mean risk
+
+        # Atrribute name having the highest single attribute mean risk
+        attribute_with_max_risk = attributes[single_attribute_risks.index(max_single_attri_risk)]
+
+        print(single_attribute_risks)
+        print(max_single_attri_risk)
+        print(attribute_with_max_risk)
+
+    calc_single_attribute_risks(attributes)
+
+
+
 
 
 
